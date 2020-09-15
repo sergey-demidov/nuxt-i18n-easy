e4a41e5d4144cb7e0e20caf4d31e5d267903f23f
Sergey Demidov <sergey.k.demidov@gmail.com>
2020-09-15 07:01:55 +0300

    add cli test

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,5 +1,5 @@
-// const fs = require('fs')
-// const inquirer = require('inquirer')
+const fs = require('fs')
+const inquirer = require('inquirer')
 // const mockInquirer = require('mock-inquirer')
 const lib = require('../lib/includes')
 
@@ -20,38 +20,41 @@ const lib = require('../lib/includes')
 //   })
 // })
 
-// describe('files not exist', () => {
-//   const backup = {}
-//   // jest.mock('inquirer')
-//   beforeAll(() => {
-//     backup.prompt = inquirer.prompt
-//     inquirer.prompt.prompts.confirm = () => { return Math.random() < 0.5 }
-//     // inquirer.prompt.prompts.confirm = () => Promise.resolve(() => { return Math.random() < 0.5 ? { dir: true } : { dir: false } })
-//     // inquirer.prompt. = () => { return Math.random() < 0.5 ? { dir: true } : { dir: false } }
-//     // inquirer.prompt.prompts.confirm = () => Promise.resolve({ dir: true })
-//     console.dir(inquirer.prompt.prompts.confirm)
-//     backup.mkdirSync = fs.mkdirSync
-//     fs.mkdirSync = () => {}
-//     backup.writeFileSync = fs.writeFileSync
-//     fs.writeFileSync = () => {}
-//     fs.existsSync = () => { return Math.random() < 0.5 }
-//   })
-//
-//   it('should equal test', async () => {
-//     // expect.assertions(1);
-//     for (let i = 0; i < 10; i++) {
-//       await expect(lib.checkFiles({ langDir: 'test/mocks/XXX/', locales: [{ file: 'en.js' }] })).toBeTruthy()
-//       await expect(lib.checkFiles({ langDir: 'test/mocks/lang/', locales: [{ file: 'ru.js' }] })).toBeTruthy()
-//     }
-//   })
-//
-//   // restore
-//   afterAll(() => {
-//     inquirer.prompt = backup.prompt
-//     fs.writeFileSync = backup.writeFileSync
-//     fs.mkdirSync = backup.mkdirSync
-//   })
-// })
+describe('files not exist', () => {
+  const backup = {}
+  //   // jest.mock('inquirer')
+  beforeAll(() => {
+    //     backup.prompt = inquirer.prompt
+    //     // inquirer.prompt.prompts.confirm = () => Promise.resolve(() => { return Math.random() < 0.5 ? { dir: true } : { dir: false } })
+    //     // inquirer.prompt. = () => { return Math.random() < 0.5 ? { dir: true } : { dir: false } }
+    //     // inquirer.prompt.prompts.confirm = () => Promise.resolve({ dir: true })
+    //     console.dir(inquirer.prompt.prompts.confirm)
+    //     backup.mkdirSync = fs.mkdirSync
+    //     fs.mkdirSync = () => {}
+    //     backup.writeFileSync = fs.writeFileSync
+    //     fs.writeFileSync = () => {}
+    backup.existsSync = fs.existsSync()
+
+    fs.existsSync = () => { return Math.random() < 0.5 }
+  })
+
+  test('should equal test', () => {
+    inquirer.prompt.prompts.confirm = () => { return Math.random() < 0.5 }
+    // expect.assertions(1);
+    for (let i = 0; i < 10; i++) {
+      expect(lib.checkFiles({ langDir: 'test/mocks/XXX/', locales: [{ file: 'en.js' }] })).toBeTruthy()
+      expect(lib.checkFiles({ langDir: 'test/mocks/lang/', locales: [{ file: 'ru.js' }] })).toBeTruthy()
+    }
+  })
+  //
+  //   // restore
+  afterAll(() => {
+    //     inquirer.prompt = backup.prompt
+    //     fs.writeFileSync = backup.writeFileSync
+    //     fs.mkdirSync = backup.mkdirSync
+    fs.existsSync = backup.existsSync
+  })
+})
 
 test('files exist', () => {
   expect(lib.checkFiles()).toBeDefined()
