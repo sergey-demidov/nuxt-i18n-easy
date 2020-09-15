a5b4f0705f6a6ea860812ab68d7554e40e9ac103
Sergey Demidov <sergey.k.demidov@gmail.com>
2020-09-15 05:43:43 +0300

    fix filesearch mask

--- test/fs.test.js
+++ test/fs.test.js
@@ -3,7 +3,6 @@ const lib = require('../lib/includes')
 test('search for sentences', () => {
   expect(lib.getSentences).toBeDefined()
   expect(lib.getSentences(['./test/mocks/pages'], ['*.vue'])).toContain('“I will be translated”')
-  expect(lib.getSentences(['./test/mocks/pages'], '*.vue')).toContain('“I will be translated”')
   expect(lib.getSentences(['./test/mocks/ERROR'], ['*.js'])).toEqual([])
   expect(lib.getSentences(['./test/mocks/pages'], {})).toEqual([])
 })
